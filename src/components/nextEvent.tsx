a1156340e4542f4179dcfea5a9decba8a0ae24e6
Théo ROGER <theo.roger72@gmail.com>
2021-08-15 20:22:07 +0200

    last commit

--- src/components/nextEvent.tsx
+++ src/components/nextEvent.tsx
@@ -7,17 +7,16 @@ import {
   Select,
   Typography
   } from 'antd'
+import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
 import { nextEvent } from '../data.json'
 import { useState } from 'react'
 import './NextEvent.css'
-/*import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'*/
 
 function Next() {
   moment.locale('fr')
   const { Option } = Select
   const { Title } = Typography
   const { Meta } = Card
-  let size: number = 5
   const secondTitle = 'Prochaines épreuves'
 
   const [selectedSport, setSelectedSport] = useState<Array<String>>(
@@ -26,7 +25,6 @@ function Next() {
 
   const handleChange = (value: string[]) => {
     setSelectedSport(value)
-    return (size = value.length)
   }
 
   const sportTitle = nextEvent.map((next) => (
@@ -41,23 +39,22 @@ function Next() {
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: true,
+    nextArrow: <RightCircleOutlined />,
+    prevArrow: <LeftCircleOutlined />,
   }
   const Sports = nextEvent
     .filter((next) => selectedSport.includes(next.sportTitle))
     .map((next) => (
-      <div key={next.id}>
-        <Col span={20}>
-          <Card
-            className="carouselCard"
-            cover={<img src={next.pictureUrl} alt={next.sportTitle} />}
-          >
-            <Meta
-              title={next.sportTitle}
-              description={moment(parseInt(next.date + '000')).format('DD/MM/YYYY - HH:mm')}
-            />
-          </Card>
-        </Col>
-      </div>
+      <Card
+        key={next.id}
+        className="carouselCard"
+        cover={<img src={next.pictureUrl} alt={next.sportTitle} />}
+      >
+        <Meta
+          title={next.sportTitle}
+          description={moment(parseInt(next.date + '000')).format('DD/MM/YYYY - HH:mm')}
+        />
+      </Card>
     ))
 
   return (
@@ -81,17 +78,17 @@ function Next() {
           <Title level={2}>{secondTitle}</Title>
         </Col>
       </Row>
-      <div className="carousel-slide">
-        <Row justify={'center'}>
-          {size !== 0 ? (
+      <Row justify={'center'}>
+        {selectedSport.length === 0 ? (
+          <Col>
+            <h2>Aucune épreuve de prévue</h2>
+          </Col>
+        ) : (
+          <div className="slider">
             <Carousel {...settings}>{Sports}</Carousel>
-          ) : (
-            <Col span={20}>
-              <h2>Rien n'est selectionné</h2>
-            </Col>
-          )}
-        </Row>
-      </div>
+          </div>
+        )}
+      </Row>
     </>
   )
 }
